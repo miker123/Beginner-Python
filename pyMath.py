edde6d9c9e4f16416aade9b6e3ce4d326d9302d1
Mike R <mikeup28@gmail.com>
2017-10-17 14:32:58 -0700

    Example Math

---
+++ pyMath.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+a = 21
+b = 10
+c = 0
+
+#addition
+c = a + b
+print "Line 1 - Value of c is ", c
+
+#Addition Shortcut. Same as c=c+a
+c += a
+print "Line 2 - Value of c is ", c 
+
+#Subtraction
+c = a - b
+print "Line 2 - Value of c is ", c 
+
+
+#Sin
+c *= a
+print "Line 3 - Value of c is ", c 
+
+c /= a 
+print "Line 4 - Value of c is ", c 
+
+c  = 2
+c %= a
+print "Line 5 - Value of c is ", c
+
+c **= a
+print "Line 6 - Value of c is ", c
+
+c //= a
+print "Line 7 - Value of c is ", c
