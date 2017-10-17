db5e67494b4bba2f6d6ea54ab497d9469bd36bb3
Mike R <mikeup28@gmail.com>
2017-10-17 13:26:07 -0700

    Update loops.py

--- loops.py
+++ loops.py
@@ -30,7 +30,33 @@ elif(2==1): #else if Used when wanting to see if the value is one of a few value
 else:
 	print("Caught")
 
-#break is used
-#continue is to 
+#nesting values are loops inside loops. Like inception of loops. Loopception??	
+i = 1
+while(i < 10):
+   j = 0
+   while(j <= i):	
+	print (j)
+	
+print("\nExample of a break statement:")
+#break is used to terminates a loop statement and transfers execution to the statement immediately following the loop.
+for letter in 'Python':  
+   if letter == 'h':
+      break
+   else:
+      print ('Current Letter :', letter)
 
-#nesting values
+#continue can be used in for and while loops. 
+#When used, continue rejects all the remaining statements in the current iteration of the loop and moves the control back to the top of the loop.
+for letter in 'Python': 
+   if letter == 'h':
+      continue
+   print ('Current Letter :', letter)
+
+#pass is used when a statement is required syntactically but you do not want any command or code to execute.
+#null operation and nothing occurs when it runs
+for letter in 'Python': 
+   if letter == 'h':
+      pass
+      print ('This is pass block')
+   print ('Current Letter :', letter)
+
