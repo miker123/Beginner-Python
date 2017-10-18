0fbac9b972f1fb41da81eb05cf1da03221183328
Mike R <mikeup28@gmail.com>
2017-10-18 12:39:12 -0700

    Learning about functions

--- function.py
+++ function.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 
 # Function definition is here
-def printme( str ):
+def printMe( str ):
+   newData="test" #Object newData is now local to the printMe function
+   global varGlobal=2 #use global to declare a global variable from inside a function
    "This prints a passed string into this function"
    print str
-   return;
+   #Python can return any python object,
+   return #returns None as nothing there tp return
 
 #Below is the main program
 # Now you can call printme function
-printme("I'm first call to user defined function!")
-printme("Again second call to the same function")
+str="I'm first call to user defined function!"
+printMe(str)
+printMe(str)
+
+#fixed parameters-->Interpreter and function will expect that specific number of variables
+#def printMe(*str) means that *str is optional (optional parameters)
+#def printMe(**kw) are named parameters.
